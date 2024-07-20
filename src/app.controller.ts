bfef5c4aceadcd62d258b91c208c16c7c63c5038
Amrit Niure <niureamrit2@gmail.com>
2024-07-20 19:50:26 +1000

    feat(husky): testing the hooks 2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,7 +8,6 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): string {return this.appService.getHello();
   }
 }
