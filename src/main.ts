29cc522710d19345713d53c318bbb53c11dfc451
Amrit Niure <niureamrit2@gmail.com>
2024-07-21 17:01:32 +1000

    feat(docker): dockerized the app for prod and dev env

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,24 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // swagger integration 
+  const PORT = process.env.PORT || 8080;
+  // swagger integration
   const config = new DocumentBuilder()
-  .setTitle('CRM-API')
-  .setDescription('Api doucmentation for CRM')
-  .setVersion('1.0')
-  .addBearerAuth(
-    {type: 'http',scheme:'bearer', bearerFormat:'JWT'},
-    'bearer'
-  )
-  .addSecurityRequirements('bearer')
-  .build()
+    .setTitle('CRM-API')
+    .setDescription('Api doucmentation for CRM')
+    .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'bearer',
+    )
+    .addSecurityRequirements('bearer')
+    .build();
 
-  const document = SwaggerModule.createDocument(app,config)
-  SwaggerModule.setup('docs',app,document) // sets up the swagger in /api path with the app and the swagger document created above.
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document); // sets up the swagger in /api path with the app and the swagger document created above.
 
-  await app.listen(3000);
+  await app.listen(PORT, () => {
+    console.log(`Running API in MODE:${process.env.NODE_ENV} on Port: ${PORT}`);
+  });
 }
 bootstrap();
